c96ec96f0cd7592d3f07c1f11d922c58fd0cc3c7
Wyatt-James <bomberbeast44@gmail.com>
2026-03-10 16:59:45 -0500

    mark fxtest_swap and fxtest_not pass commits

--- source/Snes9x/fxinst_tests.cpp
+++ source/Snes9x/fxinst_tests.cpp
@@ -131,7 +131,7 @@ FX_Result fxtest_loop(const FX_Gsu* GSUi, const uint16 r12)
     return packResult(GSU, resultNew, resultOld);
 }
 
-// Passed in commit WYATT_TODO
+// Passed in commit 767428a
 FX_Result fxtest_swap(const FX_Gsu* GSUi, const uint16 v1)
 {
     FX_Gsu GSU = *GSUi;
@@ -149,7 +149,7 @@ FX_Result fxtest_swap(const FX_Gsu* GSUi, const uint16 v1)
     return packResult(GSU, resultNew, resultOld);
 }
 
-// Passed in commit WYATT_TODO
+// Passed in commit 767428a
 FX_Result fxtest_not(const FX_Gsu* GSUi, const uint16 v1)
 {
     FX_Gsu GSU = *GSUi;
